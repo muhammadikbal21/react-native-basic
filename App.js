35c989b8f95e6733516ada9d16b73e7c3bee900e
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-06-11 21:02:54 +0700

    styling View component

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { Text, View } from 'react-native'
 const App = () => {
   return (
     <View>
+      <View style={{width: 80, height: 80, backgroundColor: '#0abde3'}} />
       <Text>Muhammad Ikbal</Text>
       <BalComponent />
     </View>
