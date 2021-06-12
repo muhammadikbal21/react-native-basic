86dc18e228e371c300d35fcd309d213e0246073d
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-06-12 09:11:36 +0700

    styling basic

--- App.js
+++ App.js
@@ -1,7 +1,41 @@
 import React, { Component } from 'react'
-import { Image, Text, TextInput, View } from 'react-native'
+import { Image, StyleSheet, Text, TextInput, View } from 'react-native'
 
 const App = () => {
+  return (
+    <StylingReactNativeComponent />
+  )
+}
+
+const StylingReactNativeComponent = () => {
+  return (
+    <View>
+      <Text style={style.text}>Styling Component</Text>
+      <View style={{
+        width: 100,
+        height: 100,
+        backgroundColor: 'blue',
+        borderWidth: 2,
+        borderColor: 'red',
+        marginTop: 20,
+        marginLeft: 20
+      }}
+    />
+    </View>
+  )
+}
+
+const style = StyleSheet.create({
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'green',
+    marginLeft: 20,
+    marginTop: 40
+  }
+})
+
+const SampleComponent = () => {
   return (
     <View>
       <View style={{width: 80, height: 80, backgroundColor: '#0abde3'}} />
