dd840efe47ad7445539df72a887a9ae0d057f17b
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-06-12 08:53:28 +0700

    add class BoxGreen and class Profile

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Image, Text, TextInput, View } from 'react-native'
 
 const App = () => {
@@ -9,6 +9,8 @@ const App = () => {
       <BalComponent />
       <Photo />
       <TextInput style={{borderWidth: 1}} />
+      <BoxGreen />
+      <Profile />
     </View>
   )
 }
@@ -19,6 +21,23 @@ const BalComponent = () => {
 
 function Photo() {
   return <Image source={{uri: 'https://placeimg.com/100/100/tech'}} style={{width: 100, height: 100}} />
+}
+
+class BoxGreen extends Component {
+  render() {
+    return <Text>Ini component dari class</Text>
+  }
+}
+
+class Profile extends Component {
+  render() {
+    return (
+      <View>
+        <Image source={{uri: 'https://placeimg.com/100/100/nature'}} style={{width: 100, height: 100, borderRadius: 50}} />
+        <Text style={{color: 'blue', fontSize: 24}}>Ini Hewan</Text>
+      </View>
+    ) 
+  }
 }
 
 export default App
